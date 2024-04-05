cba253b6e2b6722ad361ba37914acb1a822d0ec5
Yuberley <yuberleyguerrero1999@gmail.com>
2024-04-05 08:49:41 -0500

    Stated layer application

--- src/Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
+++ src/Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
@@ -1,6 +1,18 @@
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Users;
+
 namespace Bookify.Application.Bookings.ReserveBooking;
 
-public class ReserveBookingCommandHandler
+internal sealed class ReserveBookingCommandHandler : ICommandHandler<ReserveBookingCommand, Guid>
 {
+    private readonly IUserRepository _userRepository;
+    private readonly IApartmentRepository _apartmentRepository;
+    private readonly IBookingRepository _bookingRepository;
     
+    public Task<Result<Guid>> Handle(ReserveBookingCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
